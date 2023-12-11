9ed32fc02472f25f698dd8eebfcebc25c873945b
Ian Nakamura <16380276688@estudante.ufjf.br>
2023-12-10 21:32:57 -0300

    Cria a função leFormulario no main.js

--- main.js
+++ main.js
@@ -3,3 +3,28 @@ import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
 const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
 
+document.entrada.addEventListener('submit', leFormulario);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
+
+atualizaTela();
+
+function leFormulario(event) {
+    event.preventDefault(event);
+    const quantidade = document.entrada.quantidade.valueAsNumber;
+    const fruta = document.entrada.fruta.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
+    atualizaTela();
+}
+
+
